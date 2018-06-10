70a377d51b8d47fd222d9f8c2917c5b30ba1cf97
Sam Newbold <sam@rwsh.org>
2018-06-10 15:08:08 +0200

    Add setuptools packaging files
    ... leaving local directory structure

---
+++ id_trust/README.rst
@@ -0,0 +1,11 @@
+=======================
+Identification of Trust
+=======================
+
+This is the implementation of the Identification of Trust microsite.
+
+Installation
+INSTALLED_APPS += 'id_trust.apps.IdTrustConfig'
+urlpatterns += path('identification_of_trust/', include('id_trust.urls'))
+
+
---
+++ id_trust/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='id_trust',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Not licensed - contact the author if you are not the author',
+    description='A microsite to explore identifying game-theory strategies',
+    long_description=README,
+    install_requires=['Django>=2.0,<2.1', 'django-choices>=1.6,<1.7'],
+    url='https://www.example.com/id_trust',
+    author='Samuel Newbold',
+    author_email='sam@rwsh.org',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
