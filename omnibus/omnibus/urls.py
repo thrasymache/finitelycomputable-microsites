4df23959f588747061a6954b5eed6e2390ef8d65
Sam Newbold <sam@rwsh.org>
2018-06-10 21:51:54 +0200

    Reorganized for id_trust to be reusable in omnibus
    which is a better name for a project that will be one of many

--- omnibus/omnibus/urls.py
+++ omnibus/omnibus/urls.py
@@ -1,4 +1,4 @@
-"""dj_hobby URL Configuration
+"""omnibus URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
